4385514095af3e51cdb3e3177c9bd09cfcedd800
Dan Mercer <danielmercer1@gmail.com>
2022-04-30 17:00:35 -0600

    Extract debug stuff into Debug component

---
+++ src/components/Debug.tsx
@@ -0,0 +1,53 @@
+import { getDurations, getDurationMagnitudeMs } from "../store/analytics";
+import { getEvents, nukeAllEvents } from "../store/store";
+import { includesProperties } from "../util/object";
+import { useTime, useAsyncGetter } from "../util/react/hooks";
+import { PopupApp } from "./PopupApp";
+
+export const Debug = () => {
+  const time = useTime();
+  const [events, , refresh] = useAsyncGetter(getEvents);
+  const durations = events && getDurations(
+    events,
+    includesProperties({ focused: true }),
+    includesProperties({ focused: false }),
+  )
+
+  return <div>
+    <div>
+      <details open>
+        <summary>Event debugger:</summary>
+        <button onClick={refresh}>Refresh</button>
+        <button onClick={nukeAllEvents}>Clear events</button>
+        <p>Events:</p>
+        <ul>
+          {events?.map((e, i) => {
+            const di = durations.findIndex(d => d.start === e || d.end === e);
+            const d = durations[di];
+            const isStart = d?.start === e;
+            const isEnd = d?.end === e;
+            const magnitude = d ? Math.round(getDurationMagnitudeMs(d) / 1000) + 's' : '';
+            return <li style={{
+              color: isStart ? 'red' : isEnd ? 'green' : '',
+            }} key={String(i)}>
+              {JSON.stringify(e) + ' ' + (d ? `index:${di}` : '') + ' ' + magnitude}
+            </li>
+          })}
+        </ul>
+        <p>Durations:</p>
+        <ul>
+          {durations?.map((d, i) => {
+            return <li key={String(i)}>{JSON.stringify(d)}</li>
+          })}
+        </ul>
+        <p>
+          Current time: {time.toString()}
+        </p>
+      </details>
+    </div>
+    <h1>Popup preview (static - does not refresh)</h1>
+    <div className="OptionsApp-popup-preview">
+      <PopupApp></PopupApp>
+    </div>
+  </div>
+}
